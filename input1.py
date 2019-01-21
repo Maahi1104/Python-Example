f97d58d21365442420181532e1d49502154a5e79
Maahi1104 <46883637+Maahi1104@users.noreply.github.com>
2019-01-21 16:08:37 +0530

    Create input1.py

---
+++ input1.py
@@ -0,0 +1,5 @@
+#This is the example of taking input from the user.
+name=input("Enter your name\n")
+print("My name is",name)
+registration=int(input("Enter Registration No.\n"))
+print("My Registration no. is",registration)
