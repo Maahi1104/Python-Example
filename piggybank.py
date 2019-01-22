108e2b22bf59fc7ea2068d7d132d3c7d80cc9d96
Maahi1104 <46883637+Maahi1104@users.noreply.github.com>
2019-01-22 15:56:59 +0530

    Add files via upload

---
+++ piggybank.py
@@ -0,0 +1,22 @@
+print("           PIGGY Bank         ")
+print("**********************************************")
+a=int(input("enter the number of coins for 10rs: "))
+b=int(input("enter the number of coins for 5rs: "))
+c=int(input("enter the number of coins for 2rs: "))
+d=int(input("enter the number of coins for 1rs: "))
+
+multia= a*10
+multib= b*5
+multic= c*2
+multid= d*1
+ 
+ 
+print("Total of 10rs= ", multia)
+print("Total of 5rs= ", multib)
+print("Total of 2rs= ", multic)
+print("Total of 1rs= ", multid)
+
+sum= multia + multib + multic + multid
+
+print("Total Amount of Money: ",sum)
+print("**********************************************")
